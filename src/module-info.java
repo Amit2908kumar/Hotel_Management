314090a2d851b0716d0741252b2bc0d8a016a423
Amit2908kumar <amit.kumar2020b@vitstudent.ac.in>
2024-07-31 23:28:09 +0530

    init

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module hmamdocs {
-	requires java.sql;
-    exports com.hotelmanagement.util;
-    exports com.hotelmanagement.dao;
-    exports com.hotelmanagement.model;
-    exports com.hotelmanagement.service;
-    exports com.hotelmanagement.controller;
-    exports com.hotelmanagement.main;
-}
